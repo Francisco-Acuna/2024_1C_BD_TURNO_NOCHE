62ab894b0f2dd6cfdb2d69d89d08e1db5bf4fce1
Francisco-Acuna <140303345+Francisco-Acuna@users.noreply.github.com>
2024-03-13 22:11:25 -0300

    Add files via upload

---
+++ Clase01.sql
@@ -0,0 +1,34 @@
+-- ------------------------------------
+-- Curso Base de Datos con MySQL
+-- Trayecto Programador
+-- ------------------------------------
+
+-- comentario en línea
+-- es necesario dejar un espacio luego de los --
+
+/*
+bloque de
+comentarios
+se puede escribir en varias
+líneas
+*/
+
+-- ANSI SQL. El ANSI es un estándar del lenguaje
+-- Funciona en todas las bases de datos relacionales
+
+# comentario no ANSI. Sólo funciona en MySQL
+
+-- SQL -> Structure Query Language (Lenguaje de Consulta Estructurada)
+
+/*
+DDL -> Data Definition Language (Lenguaje de Definición de Datos)
+- CREATE
+- ALTER
+- DROP
+*/
+
+
+
+
+
+
